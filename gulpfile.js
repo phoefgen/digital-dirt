993c369f2e171dc6d054c8f106cca2c7e171c708
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2017-10-20 16:07:16 -0700

    add build to project

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+// function reference: https://markgoodyear.com/2014/01/getting-started-with-gulp/
+
+// To install requirements: `npm install gulp-ruby-sass gulp-autoprefixer gulp-cssnano gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del gulp-pug pug --save-dev`
+
+
+/*****************************************************************************/
+// setup env. 
+
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    pug = require('gulp-pug');
+
+/*****************************************************************************/
+// Setup workflow tasks
+
+gulp.task('styles', function() {
+  return sass('src/styles/*.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('pug', function() {
+  return gulp.src('src/html/**/*.pug', function() {
+    .pipe(pug())
+    .pipe(gulp.dest('dist/assets/html'));
+}
+
+gulp.task('clean', function() {
+    return del(['dist/assets/html', 'dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+});
+
+
+
+/*****************************************************************************/
+// Helper Functions. 
+
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'pug', 'scripts', 'images');
+});
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+  // Watch pug files
+  gulp.watch('src/html/*.html', ['markup']);
+
+});
+
