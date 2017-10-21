81c4b6a7a215bf97a2ebe564d70b5511fa8b7b69
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2017-10-20 17:49:09 -0700

    make auto page reload work, conf pug

--- gulpfile.js
+++ gulpfile.js
@@ -7,73 +7,84 @@
 // setup env.
 
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del'),
-    pug = require('gulp-pug');
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
+  del = require('del'),
+  browserSync = require('browser-sync').create();
+pug = require('gulp-pug');
 
 /*****************************************************************************/
 // Setup workflow tasks
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return sass('src/styles/*.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('dist/assets/css'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(cssnano())
     .pipe(gulp.dest('dist/assets/css'))
+    .pipe(browserSync.stream())
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src('src/scripts/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/assets/js'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(gulp.dest('dist/assets/js'))
+    .pipe(browserSync.stream())
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src('src/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/assets/img'))
+    .pipe(browserSync.stream())
     .pipe(notify({ message: 'Images task complete' }));
 });
 
-gulp.task('pug', function() {
+gulp.task('markup', function () {
   return gulp.src('src/html/**/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('dist/assets/html'));
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist/assets/html'))
+    .pipe(browserSync.stream())
+    .pipe(notify({ message: 'pug task complete' }));
 });
 
-gulp.task('clean', function() {
-    return del(['dist/assets/html', 'dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+gulp.task('clean', function () {
+  return del(['dist/assets/html', 'dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
 });
-
 
 
 /*****************************************************************************/
 // Helper Functions.
 
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'pug', 'scripts', 'images');
+gulp.task('default', ['clean'], function () {
+  gulp.start('styles', 'markup', 'scripts', 'images', 'watch');
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
+  // Setup watcher.
+  browserSync.init({
+    server: "./dist/assets/html",
+    index: "index.html"
+  });
 
   // Watch .scss files
   gulp.watch('src/styles/**/*.scss', ['styles']);
@@ -85,6 +96,6 @@ gulp.task('watch', function() {
   gulp.watch('src/images/**/*', ['images']);
 
   // Watch pug files
-  gulp.watch('src/html/*.html', ['markup']);
+  gulp.watch('src/html/*.pug', ['markup']);
 
 });
