46dcaab56ded58c26bb3aef7a0b444ebfafe9f6e
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2017-10-27 12:06:26 -0700

    working map and markers

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,36 +1,15 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /* Primary Entry point for dana30
 */
 
 /********************************************************************************************* */
 // Collect truck data
 /********************************************************************************************* */
-
-function createInfoWindow(truck) {
-    //sanitise optional short description
-    var short_desc = '';
-    if (truck.description_short) {
-        short_desc = truck.description_short;
-    };
-
-    return '<div id="truckDetail">\n                <h1>' + truck.name + '</h1>\n                <h3>' + short_desc + '</h3>\n                <p>' + truck.description_long + '</p>\n                ';
-}
-
-function createMarker(truck, truckMap) {
-    // Create a marker to be added to the truck object.
-    // Explicitly omit a map reference, and modify the object from the ui later
-    // to control display.
-    var marker = new google.maps.Marker({
-        title: truck.name,
-        icon: '../img/truck.png',
-        position: truck.position,
-        map: truckMap,
-        animation: google.maps.Animation.DROP
-    });
-
-    return marker;
-}
 
 function getData(cb) {
     // Data source does not support CORS, requires local cors proxy in dev, and local api proxy
@@ -43,7 +22,70 @@ function getData(cb) {
     });
 }
 
-function getTrucks(truckMap) {
+var truck = function () {
+    function truck(_truck, index) {
+        _classCallCheck(this, truck);
+
+        // translate API data
+        this.name = _truck.name;
+        this.position = { lat: _truck.last.latitude, lng: _truck.last.longitude };
+        this.email = _truck.email;
+        this.rating = _truck.rating;
+        this.description_long = _truck.description;
+        this.id = index;
+
+        // Not all trucks have short descriptions.
+        if (_truck.description_short !== null) {
+            this.description_short = _truck.description_short;
+        } else {
+            this.description_short = ' ';
+        }
+
+        // In some cases, a truck is registered but has never been on the road and does
+        // not have a 'last' attribute. Guess how many hours that took to figure out!
+        if (_truck.last) {
+            // Create a HTML string to be rendered for this truck in the infowindow.
+            this.infowindowContent = this.createInfoWindowContent();
+            this.infowindow = new google.maps.InfoWindow({ content: this.infowindowContent });
+            this.marker = this.createMarker(this.name, this.position, this.infowindow, this.id);
+        }
+    }
+
+    _createClass(truck, [{
+        key: 'createMarker',
+        value: function createMarker(name, position, infowindow, id) {
+            var marker = new google.maps.Marker({
+                title: name,
+                icon: '../img/truck.png',
+                position: position,
+                map: truckMap,
+                animation: google.maps.Animation.DROP
+            });
+
+            window.google.maps.event.addListener(marker, 'click', function () {
+                trucks().forEach(function (truck) {
+                    truck.infowindow.close(truckMap, truck.marker);
+                });
+                debugger;
+                var truck = trucks()[id];
+                debugger;
+                truck.infowindow.open(truckMap, truck.marker);
+                debugger;
+            });
+            return marker;
+        }
+    }, {
+        key: 'createInfoWindowContent',
+        value: function createInfoWindowContent() {
+            // Return constructed infowindow formatting.
+            return '<div id="truckDetail">\n                    <h1>' + this.name + '</h1>\n                    <h3>' + this.description_short + '</h3>\n                    <p>' + this.description_long + '</p>\n                    ';
+        }
+    }]);
+
+    return truck;
+}();
+
+function getTrucks() {
     // get truck data
     getData(function (data) {
         // Build an array of custom truck objects from the API data.
@@ -51,48 +93,15 @@ function getTrucks(truckMap) {
         var numVendors = nameVendors.length;
 
         for (var i = 0; i <= numVendors; i++) {
-            // In some cases, a truck is registered but has never been on the road and does
-            // not have a 'last' attribute. Guess how many hours that took to figure out.
-
-            if (data.vendors[nameVendors[i]] && 'last' in data.vendors[nameVendors[i]]) {
-                (function () {
-                    // Build a custom truck object containing the marker, and referencing the position and
-                    // details from the api call.
-                    var truck = {
-                        name: data.vendors[nameVendors[i]].name,
-                        position: {
-                            lat: data.vendors[nameVendors[i]].last.latitude,
-                            lng: data.vendors[nameVendors[i]].last.longitude
-                        },
-                        email: data.vendors[nameVendors[i]].email,
-                        rating: data.vendors[nameVendors[i]].rating,
-                        description_long: data.vendors[nameVendors[i]].description,
-                        description_short: data.vendors[nameVendors[i]].description_short
-                    };
-
-                    // create an infowindow by reference to itself
-                    var infoContent = createInfoWindow(truck);
-                    truck.infowindow = new google.maps.InfoWindow({ content: infoContent });
-
-                    // build a clickable marker.
-                    truck.marker = createMarker(truck, truckMap);
-                    truck.marker.addListener('click', function () {
-                        truck.infowindow.open(truckMap, truck.marker);
-                    });
-
-                    // add new truck object to the global array.
-                    trucks.push(truck);
-
-                    // add truck to search list
-                    $('#truckList').append('<li>' + truck.name + '</li>');
-                })();
-            }
+            trucks.push(new truck(data.vendors[nameVendors[i]], i));
         }
+        //trucks()[0].infowindow.open(truckMap, trucks()[0].marker );
     });
 }
 
-var openWindow = void 0;
-var trucks = [];
+var trucks = ko.observableArray();
+var truckMap = '';
+var infowindow = void 0;
 
 /********************************************************************************************* */
 // Map functions.
@@ -101,11 +110,13 @@ var trucks = [];
 function initMap() {
 
     // Generate a map of Vancouver
-    var truckMap = new google.maps.Map(document.getElementById('map'), {
+    truckMap = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: { lat: 49.246292, lng: -123.116226 }
     });
 
+    // Generate a single, reusable infowindow
+
     // Place food trucks on it.
-    getTrucks(truckMap);
+    getTrucks();
 }
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,37 +5,6 @@
 // Collect truck data
 /********************************************************************************************* */
 
-
-function createInfoWindow(truck) {
-    //sanitise optional short description
-    let short_desc = '';
-    if (truck.description_short) {
-        short_desc = truck.description_short
-    };
-
-    return `<div id="truckDetail">
-                <h1>${truck.name}</h1>
-                <h3>${short_desc}</h3>
-                <p>${truck.description_long}</p>
-                `;
-}
-
-function createMarker(truck, truckMap) {
-    // Create a marker to be added to the truck object.
-    // Explicitly omit a map reference, and modify the object from the ui later
-    // to control display.
-    const marker = new google.maps.Marker({
-        title: truck.name,
-        icon: '../img/truck.png',
-        position: truck.position,
-        map: truckMap,
-        animation: google.maps.Animation.DROP
-    });
-
-    return marker;
-}
-
-
 function getData(cb) {
     // Data source does not support CORS, requires local cors proxy in dev, and local api proxy
     // in production.
@@ -50,7 +19,67 @@ function getData(cb) {
         });
 }
 
-function getTrucks(truckMap) {
+class truck {
+    constructor(truck, index) {
+        // translate API data
+        this.name = truck.name;
+        this.position = { lat: truck.last.latitude, lng: truck.last.longitude };
+        this.email = truck.email;
+        this.rating = truck.rating;
+        this.description_long = truck.description;
+        this.id = index;
+
+        // Not all trucks have short descriptions.
+        if (truck.description_short !== null) {
+            this.description_short = truck.description_short;
+        } else {
+            this.description_short = ' ';
+        }
+
+        // In some cases, a truck is registered but has never been on the road and does
+        // not have a 'last' attribute. Guess how many hours that took to figure out!
+        if (truck.last) {
+            // Create a HTML string to be rendered for this truck in the infowindow.
+            this.infowindowContent = this.createInfoWindowContent();
+            this.infowindow = new google.maps.InfoWindow({ content: this.infowindowContent });
+            this.marker = this.createMarker(this.name, this.position, this.infowindow, this.id);
+        }
+    }
+
+    createMarker(name, position, infowindow, id) {
+        const marker = new google.maps.Marker({
+            title: name,
+            icon: '../img/truck.png',
+            position: position,
+            map: truckMap,
+            animation: google.maps.Animation.DROP
+        });
+
+        window.google.maps.event.addListener(marker, 'click', function () {
+            trucks().forEach(function (truck) {
+                truck.infowindow.close(truckMap, truck.marker);
+            });
+            debugger
+            const truck = trucks()[id];
+            debugger
+            truck.infowindow.open(truckMap, truck.marker);
+            debugger
+
+        });
+        return marker;
+
+    }
+    createInfoWindowContent() {
+        // Return constructed infowindow formatting.
+        return `<div id="truckDetail">
+                    <h1>${this.name}</h1>
+                    <h3>${this.description_short}</h3>
+                    <p>${this.description_long}</p>
+                    `;
+    }
+}
+
+function getTrucks() {
     // get truck data
     getData(function (data) {
         // Build an array of custom truck objects from the API data.
@@ -58,46 +87,16 @@ function getTrucks(truckMap) {
         let numVendors = nameVendors.length;
 
         for (let i = 0; i <= numVendors; i++) {
-            // In some cases, a truck is registered but has never been on the road and does
-            // not have a 'last' attribute. Guess how many hours that took to figure out.
-
-            if (data.vendors[nameVendors[i]] && 'last' in data.vendors[nameVendors[i]]) {
-                // Build a custom truck object containing the marker, and referencing the position and
-                // details from the api call.
-                const truck = {
-                    name: data.vendors[nameVendors[i]].name,
-                    position: {
-                        lat: data.vendors[nameVendors[i]].last.latitude,
-                        lng: data.vendors[nameVendors[i]].last.longitude
-                    },
-                    email: data.vendors[nameVendors[i]].email,
-                    rating: data.vendors[nameVendors[i]].rating,
-                    description_long: data.vendors[nameVendors[i]].description,
-                    description_short: data.vendors[nameVendors[i]].description_short,
-                };
-
-                // create an infowindow by reference to itself
-                const infoContent = createInfoWindow(truck);
-                truck.infowindow = new google.maps.InfoWindow({ content: infoContent });
-
-                // build a clickable marker.
-                truck.marker = createMarker(truck, truckMap);
-                truck.marker.addListener('click', function () {
-                    truck.infowindow.open(truckMap, truck.marker);
-                });
-
-                // add new truck object to the global array.
-                trucks.push(truck);
-
-                // add truck to search list
-                $('#truckList').append(`<li>${truck.name}</li>`);
-            }
+            trucks.push(new truck(data.vendors[nameVendors[i]], i));
         }
+        //trucks()[0].infowindow.open(truckMap, trucks()[0].marker );
     });
+
 }
 
-let openWindow;
-let trucks = [];
+let trucks = ko.observableArray();
+let truckMap = '';
+let infowindow;
 
 
 /********************************************************************************************* */
@@ -107,13 +106,15 @@ let trucks = [];
 function initMap() {
 
     // Generate a map of Vancouver
-    const truckMap = new google.maps.Map(document.getElementById('map'), {
+    truckMap = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: { lat: 49.246292, lng: -123.116226 },
     });
 
+    // Generate a single, reusable infowindow
+
     // Place food trucks on it.
-    getTrucks(truckMap);
+    getTrucks();
 
 }
 
